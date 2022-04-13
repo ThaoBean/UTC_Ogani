eb22da04b900d1615ad0eacccaacf8e6292dad33
thao.do <thaobean1701@gmail.com>
2022-04-13 17:58:39 +0700

    add home template

---
+++ resources/views/clientPages/brand.blade.php
@@ -0,0 +1,58 @@
+<section class="hero">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-3">
+        <div class="hero__categories">
+          <div class="hero__categories__all">
+            <i class="fa fa-bars"></i>
+            <span>All departments</span>
+          </div>
+          <ul>
+            <li><a href="#">Make up</a></li>
+            <li><a href="#">Lips</a></li>
+            <li><a href="#">Skincare</a></li>
+            <li><a href="#">Body Care</a></li>
+            <li><a href="#">Hair</a></li>
+            <li><a href="#">Tools & Brushes</a></li>
+            <li><a href="#">Perfume</a></li>
+            <li><a href="#">Others</a></li>
+            <li><a href="#">Papayaya & Crisps</a></li>
+            <li><a href="#">Oatmeal</a></li>
+            <li><a href="#">Fresh Bananas</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-lg-9">
+        <div class="hero__search">
+          <div class="hero__search__form">
+            <form action="#">
+              <div class="hero__search__categories">
+                All Categories
+                <span class="arrow_carrot-down"></span>
+              </div>
+              <input type="text" placeholder="What do yo u need?">
+              <button type="submit" class="site-btn">SEARCH</button>
+            </form>
+          </div>
+          <div class="hero__search__phone">
+            <div class="hero__search__phone__icon">
+              <i class="fa fa-phone"></i>
+            </div>
+            <div class="hero__search__phone__text">
+              <h5>0868.128.559</h5>
+              <span>support 24/7 time</span>
+            </div>
+          </div>
+        </div>
+        <div class="hero__item set-bg" data-setbg="https://i.pinimg.com/474x/1d/e6/34/1de63414b7b8e27e5ea961b4a78fcf57.jpg">
+          <div class="hero__text">
+            <span>OGANI</span>
+            <h2>Cometic <br />100% Natural</h2>
+            <p>Free Pickup and Delivery Available</p>
+            <a href="#" class="primary-btn">SHOP NOW</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
